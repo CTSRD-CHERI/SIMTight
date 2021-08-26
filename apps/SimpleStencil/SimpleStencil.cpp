84134e69f1e86847e7d03ddaaed6b15152b2e85b
Paul Metzger <s1576303@sms.ed.ac.uk>
2021-08-26 18:15:55 +0100

    Choose problem size depending on whether we run in simulation or on a FPGA + polishing.

--- apps/SimpleStencil/SimpleStencil.cpp
+++ apps/SimpleStencil/SimpleStencil.cpp
@@ -11,14 +11,8 @@
 #define DEBUG false
 
 #define STENCIL_REACH 1
-#define PROBLEM_SIZE_X 48
-#define PROBLEM_SIZE_Y 48
-#define BUF_SIZE ((PROBLEM_SIZE_X + 2 * STENCIL_REACH) * (PROBLEM_SIZE_Y + 2 * STENCIL_REACH))
 
 void populate_in_buf(int *in_buf, int x_size, int y_size) {
-  x_size = x_size + STENCIL_REACH * 2;
-  y_size = y_size + STENCIL_REACH * 2;
-
   for (int y = 0; y < y_size; ++y) {
     for (int x = 0; x < x_size; ++x) {
       // Elements on the border of the iteration space
@@ -35,9 +29,6 @@ void populate_in_buf(int *in_buf, int x_size, int y_size) {
 // Generate a 'golden output' to check if the output computed
 // by the GPU kernel is correct.
 void generate_golden_output(int *in_buf, int *golden_out, int x_size, int y_size) {
-  x_size = x_size + STENCIL_REACH * 2;
-  y_size = y_size + STENCIL_REACH * 2;
-
   for (int y = 0; y < y_size; ++y) {
     for (int x = 0; x < x_size; ++x) {
       if (x == 0 || y == 0 || x == x_size - 1 || y == y_size - 1) {
@@ -81,30 +72,46 @@ struct SimpleStencil : Kernel {
 };
 
 int main() {
-  simt_aligned int in_buf[BUF_SIZE];
-  simt_aligned int out_buf[BUF_SIZE];
-  int golden_out_buf[BUF_SIZE];
+  // Are we in simulation?
+  //bool isSim = getchar();
+  bool isSim = true;
+
+  // Vector size for benchmarking
+  int problem_size_x = 512;
+  int problem_size_y = 512;
+  if (isSim) {
+    problem_size_x = 32;
+    problem_size_y = 32;
+  }
+
+  const int buf_size_x = problem_size_x + 2 * STENCIL_REACH;
+  const int buf_size_y = problem_size_x + 2 * STENCIL_REACH;
+  const int buf_size = buf_size_x * buf_size_y;
+
+  simt_aligned int in_buf[buf_size];
+  simt_aligned int out_buf[buf_size];
+  int golden_out_buf[buf_size];
 
   // Prepare buffers
   // Zero out the ouput buffers
-  for (int i = 0; i < BUF_SIZE; ++i) {
+  for (int i = 0; i < buf_size; ++i) {
     out_buf[i] = 0;
     golden_out_buf[i] = 0;
   }
-  populate_in_buf(in_buf, PROBLEM_SIZE_X, PROBLEM_SIZE_Y);
+  populate_in_buf(in_buf, buf_size_x, buf_size_y);
   
   // Generate the golden output to check if
   // the results computed by the GPU kernel are correct (see below).
-  generate_golden_output(in_buf, golden_out_buf, PROBLEM_SIZE_X, PROBLEM_SIZE_Y);
+  generate_golden_output(in_buf, golden_out_buf, buf_size_x, buf_size_y);
 
   // Do computation on the GPU
   SimpleStencil k;
   k.blockDim.x = SIMTLanes;
   k.blockDim.y = SIMTWarps;
-  k.gridDim.x  = PROBLEM_SIZE_X / SIMTLanes + (PROBLEM_SIZE_X % SIMTLanes ? 1 : 0);
-  k.gridDim.y  = PROBLEM_SIZE_Y / SIMTWarps + (PROBLEM_SIZE_Y % SIMTWarps ? 1 : 0);
-  k.x_size     = PROBLEM_SIZE_X + 2 * STENCIL_REACH;
-  k.y_size     = PROBLEM_SIZE_Y + 2 * STENCIL_REACH;
+  k.gridDim.x  = problem_size_x / SIMTLanes + (buf_size_x % SIMTLanes ? 1 : 0);
+  k.gridDim.y  = problem_size_y / SIMTWarps + (buf_size_y % SIMTWarps ? 1 : 0);
+  k.x_size     = buf_size_x;
+  k.y_size     = buf_size_y;
   k.out_buf    = out_buf;
   k.in_buf     = in_buf;
   if (DEBUG) puts("Kernel running... ");
@@ -112,10 +119,11 @@ int main() {
   if (DEBUG) puts("Done\n");
 
   // Check result
-  bool ok = check_output(out_buf, golden_out_buf, BUF_SIZE);
+  bool ok = check_output(out_buf, golden_out_buf, buf_size_x * buf_size_y);
   puts("Self test: ");
   puts(ok ? "PASSED" : "FAILED");
   putchar('\n');
+  puts("Here\n");
 
   return 0;
 }
