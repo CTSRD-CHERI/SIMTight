d57eb2e540ed9b97d1bf3c3e6f7493bd01264241
Matthew Naylor <mn416@cam.ac.uk>
2022-11-04 06:13:54 +0000

    LRU spill

--- src/Core/SIMT.hs
+++ src/Core/SIMT.hs
@@ -334,7 +334,7 @@ makeSIMTCore config mgmtReqs memReqs memResps dramStatSigs = mdo
                   executeI_NoCap scalarCSRUnit scalarMemReqs s
               }
         , regSpillBaseAddr = let a << b = a * 2^b in REG_SPILL_BASE
-        , useRoundRobinSpill = SIMTUseRoundRobinSpill == 1
+        , useLRUSpill = SIMTUseLRUSpill == 1
         }
 
   -- Pipeline instantiation
