163b10f7ba095286c5b706447c8712e0bc0c3f8f
JianyiCheng <jc9016@ic.ac.uk>
2023-07-05 11:52:26 +0100

    Added installation script for cheribuild

---
+++ scripts/build-cheri.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------------
+#    This script builds the Cheri-LLVM library
+# --------------------------------------------------------------------
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# The absolute path to the directory of this script.
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SIMTIGHT=${SCRIPT_DIR}/..
+
+cd $SIMTIGHT/cheribuild && \
+./cheribuild.py sdk-riscv64-purecap \
+--llvm/source-directory ../llvm-project \
+--cheribsd/source-directory ../cheribsd \
+--gdb/source-directory ../gdb \
+--run/custom-qemu-path ../qemu \
+--source-root ../cheri
+
+
+# cd $SIMTIGHT/cheribuild && \
+# ./cheribuild.py sdk-riscv64-purecap \
+# --pretend \
+# --llvm/source-directory ../llvm-project \
+# --source-root ../cheri
