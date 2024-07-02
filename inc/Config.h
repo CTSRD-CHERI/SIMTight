809b17c30290ac0eb348b725113cebc03886a775
Matthew Naylor <mn416@cam.ac.uk>
2024-07-02 09:34:36 +0100

    Add FloPoCo IP

--- inc/Config.h
+++ inc/Config.h
@@ -94,7 +94,7 @@ NOTE("Enable floating-point (Zfinx) extension in both CPU and SIMT cores")
 #define EnableFP 0
 
 NOTE("Enable hard floating-point multiplier")
-#define EnableHardFPMul 1
+#define EnableHardFPMul 0
 
 NOTE("CPU configuration")
 NOTE("=================")
