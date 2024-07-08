4fe2aea5761029c6a7f5c4677bbad75cb0e38a17
Matthew Naylor <mn416@cam.ac.uk>
2024-07-08 13:34:43 +0100

    Some fixes to make quartus_dse work again

--- de10-pro-e/prepare_dse.sh
+++ de10-pro-e/prepare_dse.sh
@@ -4,7 +4,9 @@
 for VFILE in ./*.v \
              ../src/*.v \
              ../pebbles/blarney/Verilog/Altera/*.v \
-             ../pebbles/src/CHERI/Verilog/*.v
+             ../pebbles/src/CHERI/Verilog/*.v \
+             ../pebbles/src/FloatingPoint/Wrappers/*.v
+
 do
   echo set_global_assignment -name VERILOG_FILE $VFILE >> DE10_Pro.qsf
 done
